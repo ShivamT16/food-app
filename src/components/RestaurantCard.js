2f9d918746f4c27e3751d34a23e584b1e9c5b791
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-09-30 22:52:38 +0530

    cards created

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import {ON_MIND_URL, CLOUDINARY_URL} from "../utils/constants"
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const RestaurantCard = () => {
     const [ restaurantCard, setRestaurantCard ] = useState([]) 
+    const [ slide, setSlide] = useState(0)
 
     useEffect(() => {
       fetchData()
@@ -17,34 +20,52 @@ const RestaurantCard = () => {
 
     // console.log(restaurantCard[0]?.card?.card?.header.title)
 
+    const handleNext = () => {
+      setSlide(slide + 3)
+    }
+    const handlePrevious = () => {
+      setSlide(slide - 3)
+    }
+
     return(
         <div>
 
         <div>  
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+        <h1> {restaurantCard[0]?.card?.card?.header?.title} </h1> 
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-around" }}>
+          <ArrowBackIcon onClick={handlePrevious} style={{ cursor: "pointer", backgroundColor: "lightgrey", padding: ".2rem", borderRadius: "2rem"}}/>
+          <ArrowForwardIcon onClick={handleNext} style={{ cursor: "pointer", backgroundColor: "lightgrey", padding: ".2rem", borderRadius: "2rem"}}/>
+        </div>
+        </div>
+
+        <div style={{ display: "flex",gap: "1.7rem", overflow: "hidden" }}>
         {
-         <h1> {restaurantCard[0]?.card?.card?.header?.title} </h1> 
-        }
-        {
-         restaurantCard[0]?.card?.card?.imageGridCards?.info?.map((imgIcon) => 
-            <div key={imgIcon.id} style={{ display: "inline-flex", flexWrap: "wrap"}}>
-             <img style={{height: "10rem", display: "inline-flex", flexWrap: "wrap"}} src={ON_MIND_URL + imgIcon.imageId} />
+         restaurantCard[0]?.card?.card?.imageGridCards?.info?.map((imgIcon, index) => 
+            <div style={{transform: `translateX(-${slide *100}%)`, transitionDuration: "1s" }} key={imgIcon} >
+             <img alt="whats on mind" style={{height: "10.3rem"}} src={ON_MIND_URL + imgIcon.imageId} />
             </div>
          )
         }
         </div>
+        </div>
 
         <div>
-        {
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
          <h1> {restaurantCard[1]?.card?.card?.header?.title} </h1> 
-        }
-         <div style={{ display: "inline-flex", flexWrap: "wrap"}}>
+         <div style={{display: "flex", gap: "1rem", justifyContent: "space-around" }}>
+         <ArrowBackIcon style={{ cursor: "pointer", backgroundColor: "lightgrey", padding: ".2rem", borderRadius: "2rem"}}/>
+         <ArrowForwardIcon style={{ cursor: "pointer", backgroundColor: "lightgrey", padding: ".2rem", borderRadius: "2rem"}}/>
+         </div>
+        </div>
+         <div style={{ display: "flex",gap: "1.5rem", overflow: "hidden", lineHeight: ".8rem"}}>
         {
          restaurantCard[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((restaurants) => 
-            <div key={restaurants.info.id} >
-             <img style={{height: "10rem"}} src={CLOUDINARY_URL + restaurants.info.cloudinaryImageId } />
+            <div key={restaurants.info.id} style={{ transform: `translate(-00%)`, transitionDuration: "2s"}} >
+             <img alt="topRestro" style={{height: "10rem", width: "14rem",borderRadius: "1rem"}} src={CLOUDINARY_URL + restaurants.info.cloudinaryImageId } />
              <h3>{restaurants.info.name} </h3>
              <h4> {restaurants.info.avgRating} {restaurants.info.sla.slaString} </h4>
-             <p> {restaurants.info.cuisines.join(", ")} </p>
+             <p style={{width: "", whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden" }}> {restaurants.info.cuisines.join(", ")} </p>
              <p> {restaurants.info.areaName} </p>
             </div>
          )
@@ -56,11 +77,11 @@ const RestaurantCard = () => {
         {
          <h1> {restaurantCard[2]?.card?.card?.title} </h1> 
         }
-         <div style={{ display: "inline-flex", flexWrap: "wrap"}}>
+         <div style={{ display: "flex", flexWrap: "wrap",margin: "0rem 2rem", gap: "", justifyContent: "space-around" }}>
         {
          restaurantCard[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((restaurants) => 
-            <div key={restaurants.info.id} >
-             <img style={{height: "10rem"}} src={CLOUDINARY_URL + restaurants.info.cloudinaryImageId } />
+            <div key={restaurants.info.id} style={{width: "min-content"}} >
+             <img alt="onlineFood" style={{height: "8rem", width: "11rem",borderRadius: "1rem"}} src={CLOUDINARY_URL + restaurants.info.cloudinaryImageId } />
              <h3>{restaurants.info.name} </h3>
              <h4> {restaurants.info.avgRating} {restaurants.info.sla.slaString} </h4>
              {/* <p> {restaurants.info.cuisines.join(", ")} </p> */}
@@ -75,10 +96,10 @@ const RestaurantCard = () => {
         {
          <h1> {restaurantCard[6]?.card?.card?.title} </h1> 
         }
-         <div style={{ display: "inline-flex", flexWrap: "wrap"}}>
+         <div style={{ display: "flex", flexWrap: "wrap", textAlign: "center"}}>
         {
          restaurantCard[6]?.card?.card?.brands?.map((restro) => 
-            <div style={{border: "2px solid black", borderRadius: ".5rem",padding: ".5rem", margin: ".3rem"}}>
+            <div style={{border: "1px solid grey", borderRadius: ".5rem", margin: "0.3rem" , padding: "0.4rem", width: "12.5rem", whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden" }}>
              {restro.text}
             </div>
          )
@@ -90,10 +111,10 @@ const RestaurantCard = () => {
         {
          <h1> {restaurantCard[7]?.card?.card?.title} </h1> 
         }
-         <div style={{ display: "inline-flex", flexWrap: "wrap"}}>
+         <div style={{ display: "inline-flex", flexWrap: "wrap", textAlign: "center"}}>
         {
          restaurantCard[7]?.card?.card?.brands?.map((restro) => 
-            <div style={{border: "2px solid black", borderRadius: ".5rem",padding: ".5rem", margin: ".3rem"}}>
+            <div style={{border: "1px solid grey", borderRadius: ".5rem", margin: "0.3rem" , padding: "0.4rem", width: "12.5rem", whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden" }}>
              {restro.text}
             </div>
          )
