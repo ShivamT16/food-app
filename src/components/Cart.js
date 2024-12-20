690bd261d23efb2eb79092718502d83aa9d1e498
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-12-20 16:11:22 +0530

    fix: Cart Page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,23 +1,37 @@
 import { useContext } from 'react'
 import { CartContext } from '../Context/CartContext'
+import cravingImage from '../assests/media/cravings.jpg'
 import { MenuItems } from './MenuItems'
+import { Link } from 'react-router-dom'
 
 export const Cart = () => {
 
     const { state, totalPrice } = useContext(CartContext)
 
     return(
-        <div className='mx-[20%] my-10'>
+        <div className='mx-[20%] my-4'>
 
-         <div className='flex justify-between items-baseline'>
-         <p className='text-3xl font-semibold text-orange-500'>Your Orders</p>
+         <div className='flex justify-between items-baseline border-b-2 border-gray-400 pb-2'>
+
+         <p className='text-4xl font-semibold text-orange-500'>Your Orders</p>
+         { state.cart.length >= 1 &&  <>
          <p className='text-lg font-medium'>Total Items: {state.cart.length} </p>
-         <p className='text-lg font-medium'>Total Price: ₹ {totalPrice}</p>
+         <p className='text-lg font-medium'>Total Price: ₹ {totalPrice}</p> 
+         </>  }
+
          </div>
 
-        { state.cart.map((item) => 
-          <MenuItems item={item} key={item?.card?.info?.id} />
-        )}
+        {state.cart.length <= 0 ?
+        <div className='relative' >
+         <img className='h-1/3' src={cravingImage} alt='cravingImage' />
+         <p className='text-sm md:text-4xl absolute top-14 left-0 p-1 md:top-52 md:left-14 my-10 md:p-2 rounded-2xl shadow-xl text-red-600 bg-yellow-100 font-bold'>
+            Craving something Delicious ?? <Link className='underline text-green-600' to="/">Order Now</Link> </p>
+        </div>  :
+        
+        state.cart.map((item) => 
+          <MenuItems item={item} key={item?.card?.info?.id} />)
+
+        }
 
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,17 @@ export const Navbar = () => {
 
     return(
         <header className="flex justify-between border-b-2 py-2 px-10 border-[lightgray] items-center">
+        
+        <Link to='/'>    
         <img src={restroLogo} alt="logo" className="h-[4rem] rounded-xl" />
+        </Link>
+
+        <input className='border-2 rounded-xl w-1/4 p-1 text-slate-600 font-medium' placeholder='Search pizza, burger, pasta ...' />
+
         <div className='flex gap-4 mx-4'>
             {onlineStatus ? <p className='text-2xl font-semibold text-green-600'>Online</p> :
              <p className='text-2xl font-semibold text-red-600'>Offline</p>}
-
-            <p className='text-2xl font-semibold' >Search</p>
+            
             <p className='text-2xl font-semibold' >SignIn</p>
             <Link to="/cart" className='text-2xl font-semibold' >Cart</Link>
         </div>
